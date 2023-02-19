4693cca57759259f0dd4105a435c8ae41f2b58d6
tamirulalan <tamirulalan@gmail.com>
2023-02-19 17:56:16 -0500

    Week Three

---
+++ 02-express-tutorial/practiceExpress.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './new-public/index.html'))
+  })
+
+  app.get('/sample', (req, res) => {
+    res.status(200).send('This is working')
+  })
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found')
+})
+
+app.listen(3000, () => {
+  console.log('server is listening on port 3000....')
+})
